bfb2f4fa6b6e97ec77f92dd2618e865407618e6c
Ondřej Polesný <ondrejp@kentico.com>
2021-10-12 16:44:56 +0200

    Adds page to handle 404

---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout">
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+
+@import "../static/scss/variables";
+@import "../static/scss/mixins";
+
+.layout {
+    max-width: $layout-width;
+    margin-left: auto;
+    margin-right: auto;
+
+    display: flex;
+    flex-direction: column;
+}
+
+.layout > div {
+  display: flex;
+  flex-direction: row;
+}
+
+.layout > div > div {
+    margin: 0 auto;
+}
+    
+@include breakpoint-sm {
+    .layout {
+        flex-direction: row;
+    }
+}
+</style>
---
+++ pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Sidebar :isHomePage="false" />
+    <div class="content">
+      <div class="content__inner">
+          <h1>Page not found</h1>
+          <p>Sorry, the page you're looking for does not exist.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+})
+</script>
